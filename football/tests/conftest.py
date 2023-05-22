37e7635165066bd8ea0852df467fdacff4ab2c08
Dawid <dawidfirmowski@gmail.com>
2023-05-22 17:03:53 +0200

    initial of tests

--- football/tests/conftest.py
+++ football/tests/conftest.py
@@ -1,1 +1,19 @@
 import pytest
+from django.test import Client
+
+from football.models import League
+
+
+@pytest.fixture()
+def client():
+    client = Client()
+    return client
+
+
+@pytest.fixture()
+def leagues_fixture():
+    lst = []
+    lst.append(League.objects.create(name='league1'))
+    lst.append(League.objects.create(name='league2'))
+    lst.append(League.objects.create(name='league3'))
+    return lst
--- football/tests/tests.py
+++ football/tests/tests.py
@@ -1,1 +1,12 @@
 import pytest
+from django.urls import reverse
+
+
+@pytest.mark.django_db
+def test_index_view(client, leagues_fixture):
+    url = reverse('index')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.context['leagues_list'].count() == len(leagues_fixture)
+    for league in leagues_fixture:
+        assert league in response.context['leagues_list']
